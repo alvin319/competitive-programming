baedc8ba843672d9a875217454a0beeb918535a9
Alvin Deng <alvin.q.deng@utexas.edu>
2015-08-13 20:13:00 -0500

    Added some misc. questions from UIL.

---
+++ Miscellaneous/AlphaFun.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Scanner;
+import java.io.*;
+
+/**
+ * Created by Alvin on 8/7/2015.
+ */
+public class AlphaFun {
+    public static void main(String[] args) throws IOException {
+        Scanner input = new Scanner(new File("alphafun.dat"));
+        ArrayList<String> stringArrayList = new ArrayList<String>();
+        while(input.hasNext()) {
+            stringArrayList.add(input.nextLine());
+        }
+        Collections.sort(stringArrayList, new Comparator<String>() {
+            @Override
+            public int compare(String first, String second) {
+                char fourthFirst = (first.length() >= 4) ? first.charAt(3) : ' ';
+                char fourthSecond = (second.length() >= 4) ? second.charAt(3) : ' ';
+                if(first.charAt(1) != second.charAt(1))
+                    return first.charAt(1) > second.charAt(1) ? 1 : -1;
+                if(fourthFirst != fourthSecond)
+                    return fourthFirst > fourthSecond ? 1 : -1;
+                if(first.charAt(first.length()-1) != second.charAt(second.length()-1))
+                    return first.charAt(first.length()-1) > second.charAt(second.length()-1) ? 1 : -1;
+                if(first.charAt(0) != second.charAt(0))
+                    return first.charAt(0) > second.charAt(0) ? 1 : -1;
+                return first.compareTo(second);
+            }
+        });
+        for(String x : stringArrayList)
+            System.out.println(x);
+    }
+}
---
+++ Miscellaneous/ReversePals.java
@@ -0,0 +1,33 @@
+/**
+ * Created by Alvin on 8/7/2015.
+ */
+import java.util.*;
+import java.io.*;
+
+public class ReversePals {
+    public static void main(String[] args) throws IOException {
+        Scanner input = new Scanner(new File("reversepals.dat"));
+        int count = input.nextInt();
+        input.nextLine();
+        for (int i = 0; i < count; i++) {
+            String number = input.nextLine();
+            System.out.println(process(number, 1));
+
+        }
+    }
+
+    public static String process(String number, int count) {
+        String totalNum = "" + (Integer.parseInt(new StringBuffer(number).reverse().toString()) + Integer.parseInt(number));
+        boolean isPali = isPali(totalNum);
+        return isPali ? (totalNum + " " + count) : process(totalNum,count+1);
+    }
+
+    public static boolean isPali(String iter) {
+        for (int i = 0; i < iter.length(); i++) {
+            char lastChar = iter.charAt(iter.length()-1-i);
+            if(iter.charAt(i) != lastChar)
+                return false;
+        }
+        return true;
+    }
+}

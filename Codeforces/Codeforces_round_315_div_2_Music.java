2ae97ed609a11ea81ad898566ff92adde3fd5efb
Alvin Deng <alvin.q.deng@utexas.edu>
2015-08-13 20:02:03 -0500

    Init commit.

---
+++ Codeforces/Codeforces_round_315_div_2_Music.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * Created by WiNDWAY on 8/12/15.
+ */
+public class Codeforces_round_315_div_2_Music {
+    public static void main(String[] args) throws IOException {
+        Scanner input = new Scanner(System.in);
+        int loopCount = 0;
+        int total = input.nextInt();
+        int existTime = input.nextInt();
+        int downloadTime = input.nextInt();
+        while(existTime < total) {
+            existTime *= downloadTime;
+            loopCount++;
+
+        }
+        System.out.println(loopCount);
+    }
+}

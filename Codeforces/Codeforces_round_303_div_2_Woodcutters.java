4707bad07213e1a146af3bf96d042993e4b657d4
Alvin Deng <alvin.q.deng@utexas.edu>
2015-10-20 18:24:20 -0500

    Finished a solution.

--- Codeforces/Codeforces_round_303_div_2_Woodcutters.java
+++ Codeforces/Codeforces_round_303_div_2_Woodcutters.java
@@ -4,31 +4,42 @@ import java.util.*;
  * Created by Alvin on 10/20/2015.
  */
 public class Codeforces_round_303_div_2_Woodcutters {
-    public static void main(String[] args) {
+
+    public static void main(String args[]) {
         Scanner input = new Scanner(System.in);
         int count = input.nextInt();
-        LinkedList<Pair> treeList = new LinkedList<Pair>();
-        ArrayList<Integer> rangeList = new ArrayList<Integer>();
-        for(int i = 1; i <= count; i++) {
-            int x = input.nextInt();
-            rangeList.add(x);
-            treeList.add(new Pair(x, input.nextInt()));
+
+        int x[] = new int[count];
+        int h[] = new int[count];
+
+        for(int i = 0; i < x.length; i++) {
+            x[i] = input.nextInt();
+            h[i] = input.nextInt();
         }
-        Collections.sort(treeList, new Comparator<Pair>() {
-            @Override
-            public int compare(Pair o1, Pair o2) {
-                return o1.h < o2.h ? -1 : 1;
+
+        int treeCount = 1; // Fall to the left for the first tree
+        int currentPos = x[0];
+        int currentX = 0;
+
+        for(int i = 1; i < count - 1; i++) {
+
+            currentX = x[i];
+
+            if(currentX - h[i] > currentPos) { // Always cut left
+                treeCount++;
+                currentPos = currentX;
+            } else if (currentX + h[i] < x[i+1]) { // Try to cut right
+                treeCount++;
+                currentPos = currentX + h[i];
+            } else { // Don't cut
+                currentPos = currentX;
             }
-        });
-    }
-}
+        }
 
-class Pair {
-    int x;
-    int h;
+        if(count > 1) { // The last can always fall to the right to be safe
+            treeCount++;
+        }
 
-    public Pair(int x, int h) {
-        this.x = x;
-        this.h = h;
+        System.out.println(treeCount);
     }
 }

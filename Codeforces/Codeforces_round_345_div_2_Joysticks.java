9037dab66e3d065d0478ace1fb56dc0054dc4d9c
Alvin Deng <alvin.q.deng@utexas.edu>
2016-04-05 13:33:40 -0500

    Update: Updated the solution with DP

--- Codeforces/Codeforces_round_345_div_2_Joysticks.java
+++ Codeforces/Codeforces_round_345_div_2_Joysticks.java
@@ -8,20 +8,28 @@ public class Codeforces_round_345_div_2_Joysticks {
         Scanner input = new Scanner(System.in);
         int first = input.nextInt();
         int second = input.nextInt();
-        int minutes = 0;
-        while(first > 0 && second > 0) {
-            if(first == 1 && second == 1)
-                break;
-            if(first >= second) {
-                first -= 2;
-                second++;
-                minutes++;
-            } else {
-                second -= 2;
-                first++;
-                minutes++;
+        int[][] matrix = new int[102][102];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                matrix[i][j] = -1;
             }
         }
-        System.out.println(minutes);
+        matrix[1][1] = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            matrix[0][i] = 0;
+            matrix[i][0] = 0;
+        }
+        System.out.println(recursiveDP(first, second, matrix));
+    }
+
+    public static int recursiveDP(int first, int second, int[][] matrix) {
+        if(first < 0 || second < 0 || (first <= 0 && second <= 0) || first > 101 || second > 101) {
+            return 0;
+        } else if(matrix[first][second] != -1) {
+            return matrix[first][second];
+        }
+        int current = 1 + Math.max(recursiveDP(first + 1, second - 2, matrix), recursiveDP(first - 2, second + 1, matrix));
+        matrix[first][second] = current;
+        return matrix[first][second];
     }
 }

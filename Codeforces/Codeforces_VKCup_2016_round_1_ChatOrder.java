7f6ed881c30973ae6454504885ba5ca2b625812b
Alvin Deng <alvin.q.deng@utexas.edu>
2016-04-07 02:11:26 -0500

    Update: Added an unfinished solution for codeforces

---
+++ Codeforces/Codeforces_VKCup_2016_round_1_ChatOrder.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+/**
+ * Created by Alvin on 4/7/2016.
+ */
+public class Codeforces_VKCup_2016_round_1_ChatOrder {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int count = input.nextInt();
+        input.nextLine();
+        for (int i = 0; i < count; i++) {
+            String current = input.nextLine();
+        }
+    }
+}

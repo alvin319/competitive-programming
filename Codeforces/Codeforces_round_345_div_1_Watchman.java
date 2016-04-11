3f7cf2ef44a7727e1725e6769415987e2a029c00
Alvin Deng <alvin.q.deng@utexas.edu>
2016-04-10 23:53:49 -0500

    Update: Finished a solution

--- Codeforces/Codeforces_round_345_div_1_Watchman.java
+++ Codeforces/Codeforces_round_345_div_1_Watchman.java
@@ -15,27 +15,27 @@ public class Codeforces_round_345_div_1_Watchman {
             long y = input.nextLong();
             if(!xPlane.containsKey(x)) {
                 xPlane.put(x, (long)1);
-            } else {
+            } else if(xPlane.containsKey(x)){
                 xPlane.put(x, xPlane.get(x) + 1);
             }
             if(!yPlane.containsKey(y)) {
                 yPlane.put(y, (long)1);
-            } else {
+            } else if(yPlane.containsKey(y)){
                 yPlane.put(y, yPlane.get(y) + 1);
             }
             Point current = new Point(x, y);
             if(!points.containsKey(current)) {
                 points.put(current, (long)1);
-            } else {
-                points.put(current, points.get(current) + 1);
+            } else if(points.containsKey(current)){
+                points.put(current, points.get(current) + (long)1);
             }
         }
         long summation = 0;
-        for(long x : xPlane.keySet()) {
+        for(Long x : xPlane.keySet()) {
             long count = xPlane.get(x);
             summation += (count * (count - 1)) / 2;
         }
-        for(long y : yPlane.keySet()) {
+        for(Long y : yPlane.keySet()) {
             long count = yPlane.get(y);
             summation += (count * (count - 1)) / 2;
         }
@@ -54,5 +54,16 @@ class Point {
     public Point(long x, long y) {
         this.x = x;
         this.y = y;
+    }
+
+    @Override
+    public int hashCode() {
+        return (int)(x * 10 + y);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Point compare = (Point)obj;
+        return this.x == compare.x && this.y == compare.y;
     }
 }

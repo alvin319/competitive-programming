f3b4b52febbc5442af8b425f87f7531b47349ca7
Alvin Deng <alvin.q.deng@utexas.edu>
2016-04-10 23:34:13 -0500

    Update: Updated the unfinished solution

--- Codeforces/Codeforces_round_345_div_1_Watchman.java
+++ Codeforces/Codeforces_round_345_div_1_Watchman.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * Created by Alvin on 4/9/2016.
@@ -10,34 +7,52 @@ public class Codeforces_round_345_div_1_Watchman {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int pair = input.nextInt();
-        ArrayList<long[]> info = new ArrayList<>();
+        HashMap<Long, Long> xPlane = new HashMap<>();
+        HashMap<Long, Long> yPlane = new HashMap<>();
+        HashMap<Point, Long> points = new HashMap<>();
         for (int i = 0; i < pair; i++) {
-            long[] newInfo = new long[2];
-            newInfo[0] = input.nextLong();
-            newInfo[1] = input.nextLong();
-            info.add(newInfo);
-        }
-        Collections.sort(info, new Comparator<long[]>() {
-            @Override
-            public int compare(long[] o1, long[] o2) {
-                return new Long(o1[0]).compareTo(o2[0]);
+            long x = input.nextLong();
+            long y = input.nextLong();
+            if(!xPlane.containsKey(x)) {
+                xPlane.put(x, (long)1);
+            } else {
+                xPlane.put(x, xPlane.get(x) + 1);
             }
-        });
-        int totalSame = 0;
-        for (int i = 0; i < info.size(); i++) {
-            for (int j = i + 1; j < info.size(); j++) {
-                long[] first = info.get(i);
-                long[] second = info.get(j);
-                if(first[0] == second[0] || first[1] == second[1]) {
-                    double manhatthan = Math.abs(first[0] - second[0]) +
-                            Math.abs(first[1] - second[1]);
-                    double distance = Math.sqrt(Math.pow(first[0] - second[0], 2) + Math.pow(first[1] - second[1], 2));
-                    if (manhatthan == distance) {
-                        totalSame++;
-                    }
-                }
+            if(!yPlane.containsKey(y)) {
+                yPlane.put(y, (long)1);
+            } else {
+                yPlane.put(y, yPlane.get(y) + 1);
+            }
+            Point current = new Point(x, y);
+            if(!points.containsKey(current)) {
+                points.put(current, (long)1);
+            } else {
+                points.put(current, points.get(current) + 1);
             }
         }
-        System.out.println(totalSame);
+        long summation = 0;
+        for(long x : xPlane.keySet()) {
+            long count = xPlane.get(x);
+            summation += (count * (count - 1)) / 2;
+        }
+        for(long y : yPlane.keySet()) {
+            long count = yPlane.get(y);
+            summation += (count * (count - 1)) / 2;
+        }
+        for(Point current : points.keySet()) {
+            long count = points.get(current);
+            summation -= (count * (count - 1)) / 2;
+        }
+        System.out.println(summation);
+    }
+}
+
+class Point {
+    long x;
+    long y;
+
+    public Point(long x, long y) {
+        this.x = x;
+        this.y = y;
     }
 }

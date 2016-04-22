a6d39644dcb130fa79d28d88ff85e2ce1efad7f5
Alvin Deng <alvin.q.deng@utexas.edu>
2016-04-22 03:43:54 -0500

    Update: Updated a solution

--- Codeforces/Codeforces_round_308_div_2_VanyaAndBooks.py
+++ Codeforces/Codeforces_round_308_div_2_VanyaAndBooks.py
@@ -1,7 +1,12 @@
 num = int(input())
 total = 0
 
-for i in range(1, num+1):
-	total += len(str(i))
+for x in range(1, 11):
+    if x == len(str(num)):
+        total += (num - 10**(x - 1) + 1) * x
+    elif x < len(str(num)):
+        total += (9 * 10**(x - 1)) * x
+    else:
+        break
 
 print(total)

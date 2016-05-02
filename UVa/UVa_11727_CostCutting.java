d061993ce5ec4903082b4d271fd46c27d10e7a61
Alvin Deng <alvin.q.deng@utexas.edu>
2016-05-01 22:44:06 -0500

    Update: Added an easy UVa problem

---
+++ UVa/UVa_11727_CostCutting.java
@@ -0,0 +1,67 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+/**
+ * Created by Alvin on 4/23/2016.
+ */
+
+public class UVa_11727_CostCutting {
+    public static void main(String[] args) {
+        FScanner input = new FScanner();
+        out = new PrintWriter(new BufferedOutputStream(System.out), true);
+        int count = input.nextInt();
+        for (int i = 0; i < count; i++) {
+            int[] array = {input.nextInt(), input.nextInt(), input.nextInt()};
+            Arrays.sort(array);
+            out.println("Case " + (i + 1) + ": " + array[1]);
+        }
+        out.close();
+    }
+
+    public static PrintWriter out;
+
+    public static class FScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FScanner() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        private String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        public String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+    }
+}
+

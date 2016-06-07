b7e28382194c6367ca4e7da0ee0c796d7f9ef700
Alvin Deng <alvin.q.deng@utexas.edu>
2016-06-06 23:58:28 -0500

    Update: Added a Codeforce Solutions

---
+++ Codeforces/Codeforces_Educational_round_1_TrickySum.java
@@ -0,0 +1,71 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+/**
+ * Created by Alvin on 6/6/2016.
+ */
+
+public class Codeforces_Educational_round_1_TrickySum {
+    public static void main(String[] args) {
+        FScanner input = new FScanner();
+        out = new PrintWriter(new BufferedOutputStream(System.out), true);
+        int n = input.nextInt();
+        for (int i = 0; i < n; i++) {
+            int current = input.nextInt();
+            BigInteger answer = BigInteger.ZERO;
+            for (int j = 1; j <= current; j++) {
+                System.out.println(Math.log(j));
+            }
+        }
+        out.close();
+    }
+
+    public static boolean isPower
+
+    public static PrintWriter out;
+
+    public static class FScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FScanner() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        private String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        public String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+    }
+}
+

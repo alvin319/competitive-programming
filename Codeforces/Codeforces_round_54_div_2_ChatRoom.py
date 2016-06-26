a7f14836ee8f09f9915e275d316cddb2617c839e
Alvin Deng <qdeng@apple.com>
2016-06-26 14:36:00 +0100

    Fix: Fixed a solution for Codeforces

--- Codeforces/Codeforces_round_54_div_2_ChatRoom.py
+++ Codeforces/Codeforces_round_54_div_2_ChatRoom.py
@@ -6,12 +6,9 @@ index = 0
 
 for i in range(len(current_s)):
 	if current_s[i] == array[index]:
-		print(array[index])
 		index += 1
-		print(current_s[i: ])
 	if index == len(array):
 		print("YES")
 		quit()
 
 print("NO")
-quit()

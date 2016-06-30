dec6324de3a53bc9ff441d70473fb0759a9adc46
Alvin Deng <alvin.q.deng@utexas.edu>
2016-06-30 14:00:00 +0100

    Update: Added a solution for Codeforces

---
+++ Codeforces/Codeforces_round_173_div_2_BitPlusPlus.java
@@ -0,0 +1,71 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+/**
+ * Created by Alvin on 7/4/2016.
+ */
+
+public class Codeforces_round_173_div_2_BitPlusPlus {
+    public static void main(String[] args) {
+        FScanner input = new FScanner();
+        out = new PrintWriter(new BufferedOutputStream(System.out), true);
+        int n = input.nextInt();
+        int x = 0;
+        for (int i = 0; i < n; i++) {
+            String current = input.nextLine();
+            if(current.equals("++X") || current.equals("X++")) {
+                x++;
+            } else {
+                x--;
+            }
+        }
+        out.println(x);
+        out.close();
+    }
+
+    public static PrintWriter out;
+
+    public static class FScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FScanner() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        private String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        public String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+    }
+}
+

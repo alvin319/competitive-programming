c70c77cd43252bf6e47aa5536c9635b6fb5049b5
Alvin Deng <alvin.q.deng@utexas.edu>
2016-07-14 00:00:37 -0500

    Update: Added a solution for Codeforces

---
+++ Codeforces/Codeforces_round_143_div_2_Team.java
@@ -0,0 +1,71 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+/**
+ * Created by Alvin on 7/13/2016.
+ */
+
+public class Codeforces_round_143_div_2_Team {
+    public static void main(String[] args) {
+        FScanner input = new FScanner();
+        out = new PrintWriter(new BufferedOutputStream(System.out), true);
+        int count = input.nextInt();
+        int total = 0;
+        for (int i = 0; i < count; i++) {
+            int s = input.nextInt();
+            int t = input.nextInt();
+            int r = input.nextInt();
+            if(s + t + r >= 2) {
+                total++;
+            }
+        }
+        out.println(total);
+        out.close();
+    }
+
+    public static PrintWriter out;
+
+    public static class FScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FScanner() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        private String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        public String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+    }
+}
+

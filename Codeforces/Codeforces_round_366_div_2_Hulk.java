0748d011ec4e1ab7afc3a1e7327222e3d7954714
Alvin Deng <alvin.q.deng@utexas.edu>
2016-08-05 14:00:00 +0100

    Update: Added a solution for Codeforces

---
+++ Codeforces/Codeforces_round_366_div_2_Hulk.java
@@ -0,0 +1,66 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+/**
+ * Created by Alvin on 8/8/2016.
+ */
+
+public class Codeforces_round_366_div_2_Hulk {
+    public static void main(String[] args) {
+        FScanner input = new FScanner();
+        out = new PrintWriter(new BufferedOutputStream(System.out), true);
+        int count = input.nextInt();
+        String answer = "I hate";
+        for (int i = 2; i <= count; i++) {
+            answer += i % 2 == 0 ? " that I love" : " that I hate";
+        }
+        out.println(answer + " it");
+        out.close();
+    }
+
+    public static PrintWriter out;
+
+    public static class FScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FScanner() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        private String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        public String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+    }
+}
+

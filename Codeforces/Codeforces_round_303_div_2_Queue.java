d979771d0f0ba218c545a17f08e15c34756a4bdd
Alvin Deng <alvin.q.deng@utexas.edu>
2016-08-29 15:49:12 -0500

    Update: Added a solution for Codeforces

---
+++ Codeforces/Codeforces_round_303_div_2_Queue.java
@@ -0,0 +1,75 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+/**
+ * Created by WiNDWAY on 8/29/16.
+ */
+
+public class Codeforces_round_303_div_2_Queue {
+
+    public static void main(String[] args) {
+        FScanner input = new FScanner();
+        out = new PrintWriter(new BufferedOutputStream(System.out), true);
+        long[] array = new long[input.nextInt()];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = input.nextLong();
+        }
+        Arrays.sort(array);
+        long answer = 0;
+        long sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] >= sum) {
+                answer++;
+                sum += array[i];
+            }
+        }
+        out.println(answer);
+        out.close();
+    }
+
+    public static PrintWriter out;
+
+    public static class FScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FScanner() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        private String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        public String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+    }
+}

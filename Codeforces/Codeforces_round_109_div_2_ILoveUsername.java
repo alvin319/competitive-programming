1c3753795c9a9ea3dbbbbcbd3840e4a2156b669d
Alvin Deng <alvin.q.deng@utexas.edu>
2016-10-25 14:00:00 +0100

    Update: Added a solution for Codeforces

---
+++ Codeforces/Codeforces_round_109_div_2_ILoveUsername.java
@@ -0,0 +1,76 @@
+/**
+ * Created by qicodeng on 10/27/16.
+ */
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Codeforces_round_109_div_2_ILoveUsername {
+    public static void main(String[] args) {
+        FScanner input = new FScanner();
+        out = new PrintWriter(new BufferedOutputStream(System.out), true);
+        int n = input.nextInt();
+        int[] array = new int[n];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = input.nextInt();
+        }
+        int best = array[0];
+        int worst = array[0];
+        int answer = 0;
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > best) {
+                answer++;
+                best = array[i];
+            } else if (array[i] < worst) {
+                answer++;
+                worst = array[i];
+            }
+        }
+        out.println(answer);
+        out.close();
+    }
+
+    public static PrintWriter out;
+
+    public static class FScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FScanner() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        private String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        public String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}

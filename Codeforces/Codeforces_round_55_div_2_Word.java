b9acc5c11ed861ae8352d25b9a47c112b5d10a85
Alvin Deng <alvin.q.deng@utexas.edu>
2016-11-11 21:47:57 -0800

    Update: Added a solution for Codeforces

---
+++ Codeforces/Codeforces_round_55_div_2_Word.java
@@ -0,0 +1,70 @@
+/**
+ * Created by qicodeng on 11/11/16.
+ */
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Codeforces_round_55_div_2_Word {
+    public static void main(String[] args) {
+        FScanner input = new FScanner();
+        out = new PrintWriter(new BufferedOutputStream(System.out), true);
+        String line = input.nextLine();
+        char[] array = line.toCharArray();
+        int low = 0;
+        int high = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (Character.isLowerCase(array[i])) {
+                low++;
+            } else {
+                high++;
+            }
+        }
+        out.println(low >= high ? line.toLowerCase() : line.toUpperCase());
+        out.close();
+    }
+
+    public static PrintWriter out;
+
+    public static class FScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FScanner() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        private String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        public String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}

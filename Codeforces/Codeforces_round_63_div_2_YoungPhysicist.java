4ee6275590f4064af47778166a2ab286c4f96125
Alvin Deng <alvin.q.deng@utexas.edu>
2016-11-25 04:39:04 -0600

    Update: Added a solution for Codeforces

---
+++ Codeforces/Codeforces_round_63_div_2_YoungPhysicist.java
@@ -0,0 +1,68 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+/**
+ * Created by WiNDWAY on 11/25/16.
+ */
+
+public class Codeforces_round_63_div_2_YoungPhysicist {
+
+    public static void main(String[] args) {
+        FScanner input = new FScanner();
+        out = new PrintWriter(new BufferedOutputStream(System.out), true);
+        int n = input.nextInt();
+        int answer = 0;
+        for (int i = 0; i < n; i++) {
+            answer += input.nextInt();
+            answer += input.nextInt();
+            answer += input.nextInt();
+        }
+        out.println(answer == 0 ? "YES" : "NO");
+        out.close();
+    }
+
+    public static PrintWriter out;
+
+    public static class FScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FScanner() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        private String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        public String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+    }
+}

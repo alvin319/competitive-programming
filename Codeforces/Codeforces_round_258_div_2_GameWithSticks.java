3fdc52229c5d1823d2ead485a2f2f8d621cb90fb
Alvin Deng <alvin.q.deng@utexas.edu>
2017-01-02 08:35:00 +0100

    Update: Added a solution for Codeforces

---
+++ Codeforces/Codeforces_round_258_div_2_GameWithSticks.java
@@ -0,0 +1,74 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+/**
+ * Created by Alvin on 1/4/2017.
+ */
+
+public class Codeforces_round_258_div_2_GameWithSticks {
+    public static void main(String[] args) {
+        FScanner input = new FScanner();
+        out = new PrintWriter(new BufferedOutputStream(System.out), true);
+        int n = input.nextInt();
+        int m = input.nextInt();
+
+        int count = 0;
+        while (n > 0 && m > 0) {
+            count++;
+            n--;
+            m--;
+        }
+
+        if (count % 2 == 1) {
+            out.println("Akshat");
+        } else {
+            out.println("Malvika");
+        }
+        out.close();
+    }
+
+    public static PrintWriter out;
+
+    public static class FScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FScanner() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        private String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        public String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
+

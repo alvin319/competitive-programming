5877043fe169083cf47bd9212c55e306f8c85deb
Alvin Deng <alvin.q.deng@utexas.edu>
2017-01-24 00:35:32 -0600

    Update: Added a solution for Codeforces

---
+++ Codeforces/Codeforces_GoodBye_2013_NewYearCandles.java
@@ -0,0 +1,70 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+/**
+ * Created by Alvin on 1/24/2017.
+ */
+
+public class Codeforces_GoodBye_2013_NewYearCandles {
+    public static void main(String[] args) {
+        FScanner input = new FScanner();
+        out = new PrintWriter(new BufferedOutputStream(System.out), true);
+        int a = input.nextInt();
+        int b = input.nextInt();
+        int e = a;
+        while (a >= b) {
+            int r = a % b;
+            a = (a / b);
+            e += a;
+            a += r;
+        }
+        out.println(e);
+
+        out.close();
+    }
+
+    public static PrintWriter out;
+
+    public static class FScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FScanner() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        private String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        public String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
+

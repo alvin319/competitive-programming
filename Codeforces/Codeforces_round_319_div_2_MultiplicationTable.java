affe785ec28ac25b72cc410e685daaad6769b7d0
Alvin Deng <alvin.q.deng@utexas.edu>
2017-01-25 23:49:48 -0600

    Update: Added a solution for Codeforces

---
+++ Codeforces/Codeforces_round_319_div_2_MultiplicationTable.java
@@ -0,0 +1,68 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+/**
+ * Created by Alvin on 1/25/2017.
+ */
+
+public class Codeforces_round_319_div_2_MultiplicationTable {
+    public static void main(String[] args) {
+        FScanner input = new FScanner();
+        out = new PrintWriter(new BufferedOutputStream(System.out), true);
+        int n = input.nextInt();
+        int x = input.nextInt();
+        int answer = 0;
+        for (int i = 1; i <= n; i++) {
+            if (x % i == 0 && x / i <= n) {
+                answer++;
+            }
+        }
+        out.println(answer);
+        out.close();
+    }
+
+    public static PrintWriter out;
+
+    public static class FScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FScanner() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        private String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        public String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
+

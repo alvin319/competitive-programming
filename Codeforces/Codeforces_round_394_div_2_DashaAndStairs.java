75f08bc488c20a9519a0e2889c516d3135f07765
Alvin Deng <alvin.q.deng@utexas.edu>
2017-02-05 23:49:45 -0600

    Update: Added a solution for Codeforces

---
+++ Codeforces/Codeforces_round_394_div_2_DashaAndStairs.java
@@ -0,0 +1,66 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+/**
+ * Created by Alvin on 2/5/2017.
+ */
+
+public class Codeforces_round_394_div_2_DashaAndStairs {
+    public static void main(String[] args) {
+        FScanner input = new FScanner();
+        out = new PrintWriter(new BufferedOutputStream(System.out), true);
+        int a = input.nextInt();
+        int b = input.nextInt();
+        if (Math.abs(a - b) > 1 || (a == 0 && b == 0)) {
+            out.println("NO");
+        } else {
+            out.println("YES");
+        }
+        out.close();
+    }
+
+    public static PrintWriter out;
+
+    public static class FScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FScanner() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        private String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        public String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
+

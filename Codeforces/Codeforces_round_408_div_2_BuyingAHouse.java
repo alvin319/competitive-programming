04687655ff3b2c072130ec2688908ec3ccb2f2c5
Alvin Deng <alvin.q.deng@utexas.edu>
2017-03-25 18:45:00 +0100

    Update: Updated gitignore

---
+++ Codeforces/Codeforces_round_408_div_2_BuyingAHouse.java
@@ -0,0 +1,77 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Codeforces_round_408_div_2_BuyingAHouse {
+
+    public static void main(String[] args) {
+        FScanner input = new FScanner();
+        out = new PrintWriter(new BufferedOutputStream(System.out), true);
+        int n = input.nextInt();
+        int m = input.nextInt();
+        int k = input.nextInt();
+        int[] array = new int[n];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = input.nextInt();
+        }
+        int bestHouseIndex = -1;
+        int bestHouseDistance = Integer.MAX_VALUE;
+        for (int i = 0; i < array.length; i++) {
+            if (i != m - 1) {
+                if (k >= array[i] && array[i] != 0) {
+                    int distance = Math.abs(i - (m - 1));
+                    if (distance < bestHouseDistance) {
+                        bestHouseDistance = distance;
+                        bestHouseIndex = i;
+                    }
+                }
+            }
+        }
+        out.println(Math.abs(bestHouseIndex - (m - 1)) * 10);
+        out.close();
+    }
+
+    public static PrintWriter out;
+
+    public static class FScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FScanner() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        private String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        public String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+    }
+}

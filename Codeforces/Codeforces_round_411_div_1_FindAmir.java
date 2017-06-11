554119f51af16295d8463b50a530b380b16d872d
Alvin Deng <alvin.deng@drawbridge.com>
2017-06-11 09:15:00 +0100

    Update: Added a solution for Codeforces

---
+++ Codeforces/Codeforces_round_411_div_1_FindAmir.java
@@ -0,0 +1,60 @@
+/**
+ * Created by alvind on 7/7/17.
+ */
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Codeforces_round_411_div_1_FindAmir {
+    public static void main(String[] args) {
+        FScanner input = new FScanner();
+        out = new PrintWriter(new BufferedOutputStream(System.out), true);
+        int n = input.nextInt();
+        out.println(Math.round(((double) n) / 2) - 1);
+        out.close();
+    }
+
+    public static PrintWriter out;
+
+    public static class FScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FScanner() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        private String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        public String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}

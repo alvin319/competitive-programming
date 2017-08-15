4654e4f310ffc99046721275b420b05d4149ac41
Alvin Deng <alvin.q.deng@utexas.edu>
2017-08-14 23:55:39 -0700

    Update: Added a solution for Codeforces

---
+++ Codeforces/Codeforces_round_428_div_2_AryaAndBran.java
@@ -0,0 +1,76 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+
+public class Codeforces_round_428_div_2_AryaAndBran {
+
+    public static void main(String[] args) {
+        FScanner input = new FScanner();
+        out = new PrintWriter(new BufferedOutputStream(System.out), true);
+        int n = input.nextInt();
+        int k = input.nextInt();
+        int[] array = new int[n];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = input.nextInt();
+        }
+        int reqs = 0;
+        for (int i = 0; i < array.length; i++) {
+            reqs += array[i];
+            int give = Math.min(8, reqs);
+            if (k - give > 0) {
+                k -= give;
+                reqs -= give;
+            } else {
+                out.println(i + 1);
+                System.exit(0);
+            }
+        }
+        out.println(-1);
+        out.close();
+    }
+
+    public static PrintWriter out;
+
+    public static class FScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FScanner() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        private String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        public String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+    }
+}

a844fdd3dc4740efad891f44fb3d921f55ea50bb
Alvin Deng <alvin.q.deng@utexas.edu>
2017-08-19 10:15:00 +0100

    Update: Added a solution for Codeforces

---
+++ Codeforces/Codeforces_round_280_div_2_VanyaAndCubes.java
@@ -0,0 +1,72 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+
+public class Codeforces_round_280_div_2_VanyaAndCubes {
+
+    public static void main(String[] args) {
+        FScanner input = new FScanner();
+        out = new PrintWriter(new BufferedOutputStream(System.out), true);
+        int n = input.nextInt();
+        int height = 0;
+        int count = 1;
+        int total = 0;
+        while (n > 0) {
+            if (n - (total + count) >= 0) {
+                n -= (total + count);
+                total += count;
+                count++;
+                height++;
+            } else {
+                break;
+            }
+        }
+        out.println(height);
+        out.close();
+    }
+
+    public static PrintWriter out;
+
+    public static class FScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FScanner() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        private String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        public String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+    }
+}

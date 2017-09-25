0186fd5d0b76dc5ca990581eaa9a3420090f15c8
Alvin Deng <alvin.q.deng@utexas.edu>
2017-09-25 17:52:00 -0500

    Update: Added a solution for Codeforces

---
+++ Codeforces/Codeforces_MemSQL_round_1_DeclinedFinalists.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+
+public class Codeforces_MemSQL_round_1_DeclinedFinalists {
+
+    public static void main(String[] args) {
+        FScanner input = new FScanner();
+        out = new PrintWriter(new BufferedOutputStream(System.out), true);
+        int n = input.nextInt();
+        int max = -1;
+        for (int i = 0; i < n; i++) {
+            max = Math.max(max, input.nextInt());
+        }
+        out.println(Math.max(0, max - 25));
+        out.close();
+    }
+
+    public static PrintWriter out;
+
+    public static class FScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FScanner() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        private String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        public String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+    }
+}

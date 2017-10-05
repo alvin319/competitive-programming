9697dd804a97b2a62c95a6608b338fc956da919c
Alvin Deng <alvin.q.deng@utexas.edu>
2017-10-05 16:43:50 -0500

    Update: Added a solution for Codeforces

---
+++ Codeforces/Codeforces_round_438_div_2_BarkToUnlock.py
@@ -0,0 +1,15 @@
+key = input()
+k = int(input())
+
+words = []
+
+for _ in range(k):
+	words.append(input())
+
+
+for i in range(k):
+	for j in range(k):
+		if key in (words[i] + words[j]):
+			print('YES')
+			exit(0)
+print('NO')
